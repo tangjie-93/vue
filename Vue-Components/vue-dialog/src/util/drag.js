d9471980d3927717d9fdf31a18673f1bb282a6ee
tangjie <1490934761@qq.com>
2019-05-26 19:03:16 +0800

    修改了drag.js文件，将之改成以指令的形式

--- Vue-Components/vue-dialog/src/util/drag.js
+++ Vue-Components/vue-dialog/src/util/drag.js
@@ -2,7 +2,9 @@
  element:是被拖拽的dom
  parentELe:是拖拽元素活动的区域
 */
-function funDrag (element, parentEle) {
+function drag (el, binding, vnode) {
+  var element = el
+  var parentEle = window
   var params = {
     left: 0,
     top: 0,
@@ -19,70 +21,74 @@ function funDrag (element, parentEle) {
     event = event || window.event
     // 按下鼠标时的dom当前坐标
     params.currentX =
-        event.clientX || event.pageX + document.documentElement.scrollX
+      (event.clientX || event.pageX) +
+      (document.documentElement.scrollTop || document.body.scrollTop)
     params.currentY =
-        event.clientY || event.pageY + document.documentElement.scrollY
-  }
-  parentEle.onmouseup = function () {
-    params.flag = false
-    params.left = element.offsetLeft
-    params.top = element.offsetTop
-    // callback();
-  }
-  parentEle.onmousemove = function (event) {
+      (event.clientY || event.pageY) +
+      (document.documentElement.scrollLeft || document.body.scrollLeft)
+    element.onmousemove = function (event) {
     // 判断当前dom是否是可以拖动的
-    if (params.flag) {
-      event = event || window.event
-      // 移动时的dom当前坐标
-      // eslint-disable-next-line one-var
-      var nowX =
-            event.clientX || event.pageX + document.documentElement.scrollX,
-        nowY =
-            event.clientY || event.pageY + document.documentElement.scrollY
-      // dom移动的距离
-      // eslint-disable-next-line one-var
-      var disX = nowX - params.currentX,
-        disY = nowY - params.currentY
+      if (params.flag) {
+        event = event || window.event
+        // 移动时的dom当前坐标
+        // eslint-disable-next-line one-var
+        var nowX =
+          (event.clientX || event.pageX) +
+          (document.documentElement.scrollTop || document.body.scrollTop),
+          nowY =
+          (event.clientY || event.pageY) +
+          (document.documentElement.scrollLeft || document.body.scrollLeft)
+        // dom移动的距离
+        // eslint-disable-next-line one-var
+        var disX = nowX - params.currentX,
+          disY = nowY - params.currentY
         // 确定dom移动后的位置
-      var left = parseInt(params.left) + disX
-      var top = parseInt(params.top) + disY
-      /// /计算拖拽元素（element）距离拖拽容器左边的距离
-      if (left < 0) {
-        left = 0
-      }
-      var isWindow = parentEle === window
-      var offSetLeft = 0
-      if (isWindow) {
-        offSetLeft = window.innerWidth - element.offsetWidth
-        if (left > offSetLeft) {
-          left = offSetLeft
+        var left = parseInt(params.left) + disX
+        var top = parseInt(params.top) + disY
+        /// /计算拖拽元素（element）距离拖拽容器左边的距离
+        if (left < 0) {
+          left = 0
         }
-      } else {
-        offSetLeft = parentEle.offsetWidth - element.offsetWidth
-        if (left > offSetLeft) {
-          left = offSetLeft
+        var isWindow = parentEle === window
+        var offSetLeft = 0
+        if (isWindow) {
+          offSetLeft = window.innerWidth - element.offsetWidth
+          if (left > offSetLeft) {
+            left = offSetLeft
+          }
+        } else {
+          offSetLeft = parentEle.offsetWidth - element.offsetWidth
+          if (left > offSetLeft) {
+            left = offSetLeft
+          }
         }
-      }
-      // 计算拖拽元素（element）距离拖拽容器的高度
-      if (top < 0) {
-        top = 0
-      }
-      var offSetTop = 0
-      if (isWindow) {
-        offSetTop = window.innerHeight - element.offsetHeight
-        if (top > offSetTop) {
-          top = offSetTop
+        // 计算拖拽元素（element）距离拖拽容器的高度
+        if (top < 0) {
+          top = 0
         }
-      } else {
-        offSetTop = parentEle.offsetHeight - element.offsetHeight
-        if (top > offSetTop) {
-          top = offSetTop
+        var offSetTop = 0
+        if (isWindow) {
+          offSetTop = window.innerHeight - element.offsetHeight
+          if (top > offSetTop) {
+            top = offSetTop
+          }
+        } else {
+          offSetTop = parentEle.offsetHeight - element.offsetHeight
+          if (top > offSetTop) {
+            top = offSetTop
+          }
         }
+        element.style.left = left + 'px'
+        element.style.top = top + 'px'
       }
-
-      element.style.left = left + 'px'
-      element.style.top = top + 'px'
+    }
+    element.onmouseup = function () {
+      params.flag = false
+      params.left = element.offsetLeft
+      params.top = element.offsetTop
+      element.onmousemove = null
+      element.onmouseup = null
     }
   }
 }
-export default funDrag
+export default drag
