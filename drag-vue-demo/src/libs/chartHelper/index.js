ed1d237114b73135af59126c8127b3f5062da825
tangjie <1490934761@qq.com>
2020-08-23 01:00:40 +0800

    del darg-vue-demo

--- drag-vue-demo/src/libs/chartHelper/index.js
+++
@@ -1,5 +0,0 @@
-import BarChart from './bar'
-import LineChart from './line';
-import PieChart from './pie';
-import CustomTable from './table'
-export { BarChart, LineChart, PieChart, CustomTable }
